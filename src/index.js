0c8f9dfb39ca4a4947128c3dad89bbe842a949cb
Vaidik1317 <useremail>
2025-02-10 15:22:54 +0530

    product gallary local storage with fs

--- src/index.js
+++ src/index.js
@@ -7,12 +7,30 @@ const path = require("path");
 const fs = require("fs");
 const { dbSync } = require("./sync");
 const router = express.Router();
+const http = require("http");
+const formidable = require("formidable");
+
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 7000;
 
+http.createServer(function (req, res) {
+  if (req.url == "./uploads") {
+    let form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      let filePath = files.filetoupload.filepath;
+      let newPath = "C:/Users/Your Name/" + files.filetoupload.originalFilename;
+
+      fs.rename(filePath, newPath, function () {
+        res.write("file uploaded");
+        res.end();
+      });
+    });
+  }
+});
 app.get("/", function (req, res) {
   res.send("Welcome toFoodies choice");
 });
